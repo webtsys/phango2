9022f10c4ed7ab2fe41b80cae0055330c606bc38
Antonio de la Rosa <webmaster@web-t-sys.com>
2014-11-15 04:15:13 +0100

    Fixes on make_fancy_url

--- libraries/utilities/set_admin_link.php
+++ libraries/utilities/set_admin_link.php
@@ -3,12 +3,14 @@
 function set_admin_link($text_admin, $parameters)
 {
 	
-	if(!isset($parameters['IdModule']))
+	/*if(!isset($parameters['IdModule']))
 	{
 		$parameters['IdModule']=$_GET['IdModule'];
-	}
+	}*/
 	
-	return make_fancy_url(PhangoVar::$base_url, ADMIN_FOLDER, 'index', $text_admin, $parameters);
+	base_url/ADMIN_FOLDER/module_name/other_parameters
+	
+	return make_fancy_url(PhangoVar::$base_url, ADMIN_FOLDER, 'index', $parameters);
 
 }
 
