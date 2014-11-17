42f929144829741b86ee1af861b6c3a9b6ce0231
Antonio de la Rosa <webmaster@web-t-sys.com>
2014-11-17 04:05:48 +0100

    Fuixes on models

--- modules/users/models/models_users.php
+++ modules/users/models/models_users.php
@@ -2,10 +2,10 @@
 
 load_libraries(array('fields/passwordfield'));
 
-PhangoVar::$model['group']=new Webmodel('group');
+PhangoVar::$model['user_group']=new Webmodel('group');
 
-PhangoVar::$model['group']->set_component('name', 'CharField', array(255));
-PhangoVar::$model['group']->set_component('permissions', 'SerializeField', array());
+PhangoVar::$model['user_group']->set_component('name', 'CharField', array(255));
+PhangoVar::$model['user_group']->set_component('permissions', 'SerializeField', array());
 
 PhangoVar::$model['user']=new Webmodel('user');
 
@@ -13,10 +13,14 @@ PhangoVar::$model['user']->set_component('username', 'CharField', array(25));
 
 PhangoVar::$model['user']->set_component('password', 'PasswordField', array(25));
 
-PhangoVar::$model['user']->set_component('group', 'ForeignKeyField', array('group'));
+PhangoVar::$model['user']->set_component('group', 'ForeignKeyField', array('user_group'));
 
 PhangoVar::$model['user']->set_component('email', 'EmailField', array(255));
 
+PhangoVar::$model['login_tried']=new Webmodel('login_tried');
 
+PhangoVar::$model['login_tried']->components['ip']=new CharField(255);
+PhangoVar::$model['login_tried']->components['num_tried']=new IntegerField(11);
+PhangoVar::$model['login_tried']->components['time']=new IntegerField(11);
 
 ?>
