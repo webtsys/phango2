f7b3cba2b033796e75025764cb74b3d0aff197b3
Antonio de la Rosa <webmaster@web-t-sys.com>
2014-11-27 23:38:46 +0100

    Fixes on framework, added cli.php

---
+++ cli.php
@@ -0,0 +1,97 @@
+<?php
+
+//Loading libraries with includes, don't need more sofisticated methods...
+
+ob_start();
+
+include('classes/webmodel.php');
+include('config.php');
+include('database/'.TYPE_DB.'.php');
+
+date_default_timezone_set (MY_TIMEZONE);
+
+$utility_cli=1;
+
+//load_lang('common', 'user');
+
+$model=array();
+
+//Check arguments
+
+define('OPTS', 'm:c:');
+
+$longopts=array();
+
+$options = getopt(OPTS, $longopts);
+
+if(!isset($options['m']) && !isset($options['c']))
+{
+
+	die("Use: php cli.php -m=module -c=cli_controller [more arguments for daemon]\n");
+
+}
+
+$module=@form_text(basename($options['m']));
+
+$cli_controller=@form_text(basename($options['c']));
+
+//Connect to database
+/*
+$connection='';
+
+$connection=webtsys_connect($host_db['default'], $login_db['default'], $pass_db['default']);
+
+if(! (  $connection && webtsys_select_db($db['default']) ) )
+{
+
+	die("Error: ".webtsys_error()." - I can't connect to database\n");
+
+}
+
+//Load cache for can use load_model
+
+$query=webtsys_query(SQL_SHOW_TABLES);
+
+while(list($table)=webtsys_fetch_row($query))
+{
+
+	$arr_check_table[$table]=1;
+
+}
+
+load_model('config');
+
+$config_data=$model['config_webtsys']->select_a_row_where('');*/
+
+//Include cli_controller
+
+if(include($base_path.'modules/'.$module.'/cli/controller_'.$cli_controller.'.php'))
+{
+
+	$script_base_controller=$module;
+
+	$function_cli=$cli_controller.'Cli';
+
+	if( function_exists($function_cli) )
+	{
+	
+		$function_cli();
+	
+	}
+
+}
+else
+{
+
+	die("Error: Don't exists the controller for cli statement...\n");
+
+}
+
+function get_opts_cli($my_opts, $arr_opts=array())
+{
+
+	return getopt(OPTS.$my_opts, $arr_opts);
+
+}
+
+?>
