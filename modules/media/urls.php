4d0fe905e7b40f64fc120104423e34e147416e6c
Antonio de la Rosa <webmaster@web-t-sys.com>
2015-01-22 17:32:11 +0100

    Fix on urls for media

--- modules/media/urls.php
+++ modules/media/urls.php
@@ -1,5 +1,5 @@
 <?php
-
+/*
 PhangoVar::$urls['media']['image']=array('pattern' => '/^media\/image\/(\w+)\/([a-zA-Z0-9+]+\.{0,2})$/', 'url' => '/media/image', 'module' => 'media', 'controller' => 'index', 'action' => 'image', 'parameters' => array('$1' => 'string', '$2' => 'string'));
 
 PhangoVar::$urls['media']['css']=array('pattern' => '/^media\/css\/(\w+)\/([a-zA-Z0-9+]+\.{0,2})$/', 'url' => '/media/css', 'module' => 'media', 'controller' => 'index', 'action' => 'css', 'parameters' => array('$1' => 'string', '$2' => 'string'));
@@ -9,7 +9,8 @@ PhangoVar::$urls['media']['image_css']=array('pattern' => '/^media\/css\/images\
 PhangoVar::$urls['media']['font']=array('pattern' => '/^media\/font\/(\w+)\/([a-zA-Z0-9+]+\.{0,2})$/', 'url' => '/media/font', 'module' => 'media', 'controller' => 'index', 'action' => 'font', 'parameters' => array('$1' => 'string', '$2' => 'string'));
 
 PhangoVar::$urls['media']['jscript']=array('pattern' => '/^media\/jscript\/(\w+)\/([a-zA-Z0-9+]+\.{0,2})$/', 'url' => '/media/jscript', 'module' => 'media', 'controller' => 'index', 'action' => 'jscript', 'parameters' => array('$1' => 'string', '$2' => 'string'));
+*/
 
-PhangoVar::$urls['media']['show']=array('pattern' => '/^media\/show\/(\w+)\/(.*?)$/', 'url' => '/media/show', 'module' => 'media', 'controller' => 'showmedia', 'action' => 'show', 'parameters' => array('$1' => 'string', '$2' => 'string'));
+PhangoVar::$urls['media']['show']=array('pattern' => '/^media\/show\/(.*?)\/(.*?)$/', 'url' => '/media/show', 'module' => 'media', 'controller' => 'showmedia', 'action' => 'show', 'parameters' => array('$1' => 'string', '$2' => 'string'));
 
 ?>
